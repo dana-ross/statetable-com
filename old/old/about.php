3abc4942d5c0cf0b10249fbf56fe2b093ab64a88
Dave Ross <dave@davidmichaelross.com>
2013-02-10 02:31:20 -0500

    removing the old versions of the site

--- old/old/about.php
+++
@@ -1,8 +0,0 @@
-<?php include_once("init.php"); ?>
-<?php include_once("header.html"); ?>
-
-<p>How many times have you been working on a software project and needed a table of states &amp; provinces?  For me, it happens at least every other week.  And, each client has their own idea of which territories are important, so I can't just use the same table for all of them.</p>
-
-<p>I hope you find this site as useful as I do.  Please <a href="mailto:dave@csixty4.com">email me</a> if you have any feedback, or if you have a correction to my data.</p>
-<p>- Dave Ross</p>
-<?php include_once("footer.html"); ?>
