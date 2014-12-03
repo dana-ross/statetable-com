70daf1b7a54609778eac62b92a9e9ab0b61c7c67
Dave Ross <dave@davidmichaelross.com>
2014-12-02 20:48:33 -0500

    Grunt for script minification

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> */\n'
+			},
+			build: {
+				src: 'js/src/*.js',
+				dest: 'js/dist/<%= pkg.name %>.min.js'
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
+
+};
