8f5703dec7b2873be7e97dcc44fe35026bd0fe04
Dave Ross <dave@davidmichaelross.com>
2015-08-05 00:37:49 -0400

    Deploy script

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,14 +26,47 @@ module.exports = function(grunt) {
 					ext   : '.css'
 				}]
 			}
-		}
+		},
+                shipit: {
+                        options: {
+                                workspace: '/tmp/statetable-com',
+                                deployTo: '/srv/www/statetable.com',
+                                repositoryUrl: 'https://github.com/daveross/statetable-com.git',
+                                ignores: ['.git', 'node_modules'],
+                                keepReleases: 2,
+                                key: '/Users/dave/.ssh/id_rsa',
+                                shallowClone: true
+                        },
+                        // grunt shipit:production deploy
+                        production: {
+                                servers: 'root@davidmichaelross.com'
+                        }
+                }
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-shipit');
+        grunt.loadNpmTasks('shipit-deploy');
 
 	// Default task(s).
 	grunt.registerTask('default', ['uglify', 'sass']);
+
+        // Update Node Modules and Bower Components
+        grunt.shipit.on('updated', function() {
+                grunt.task.run([
+                        'update-node-components'
+                ]);
+        });
+
+        // update node components
+        grunt.registerTask('update-node-components', function() {
+                var     done              = this.async();
+
+                grunt.shipit.remote(['cd', grunt.shipit.releasePath, '&&', 'npm install --allow-root'].join(' ')).then(done);
+        });
+
+
 
 };
